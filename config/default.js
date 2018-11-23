73b018d0b7c312886eac6eb1a4c0f968a45dd3f7
jamma <jeff@jamma.cn>
2018-11-23 11:58:14 +0800

    自动加载所有工作模块

--- config/default.js
+++ config/default.js
@@ -1,7 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+
+const modules = {
+  kafka: {}
+}
+
+// 自动加载工作模块
+const dir = path.join(__dirname, '../packages')
+fs.readdirSync(dir)
+  .filter(file => fs.statSync(path.join(dir, file)).isDirectory() && file !== 'worker')
+  .forEach(doc => {
+    modules[doc] || (modules[doc] = {})
+  })
+
 module.exports = {
-  modules: {
-    'kafka': {},
-    'log': {},
-    'bank': {}
-  }
+  modules
 }
