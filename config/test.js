254c0faa2bae1ffd89ddc3de0a01cde917830dc6
jamma <jeff@jamma.cn>
2019-06-26 16:14:37 +0800

    config/test.js

--- config/test.js
+++ config/test.js
@@ -1,8 +1,1 @@
-module.exports = {
-  debug: true,
-  port: 3000,
-  lng: 'zh_CN',
-  groupId: 'mq_worker_dev',
-  kafka: 'kafka://test.jamma.cn:9092',
-  gateway: 'http://gateway.test.jamma.cn'
-}
+module.exports = require('./development')
